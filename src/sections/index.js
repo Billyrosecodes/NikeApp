81d790b60879c4242b2bdc7bf665586373ee6931
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-25 01:22:31 +0100

    adding in the components

---
+++ src/sections/index.js
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './SpecialOffer';
+import SpecialOffer from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    Footer
+}
