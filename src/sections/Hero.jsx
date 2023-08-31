2ef9b345dae5257228ab7180245837b50c7ae252
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-08-31 04:35:30 +0100

    adding in the PopularProductCard

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,6 @@
 import Button from "../components/Button";
+
+import { useState } from "react";
 import { arrowRight } from "../assets/icons";
 
 import { shoes, statistics } from "../constants";
@@ -6,6 +8,8 @@ import { bigShoe1 } from "../assets/images";
 import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
@@ -45,28 +49,34 @@ const Hero = () => {
           {statistics.map((stat) => (
             <div key={stat.label}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero
-      bg-center">
-        <img src={bigShoe1} alt="shoe collection"
-        width={610} 
+      <div
+        className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero
+      bg-center"
+      >
+        <img
+          src={bigShoeImg}
+          alt="shoe collection"
+          width={610}
           height={500}
           className="object-contain relative z-10"
         />
       </div>
 
-      <div>
+      <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
         {shoes.map((shoe) => (
           <div key={shoe}>
-            <ShoeCard 
+            <ShoeCard
               imgURL={shoe}
-              changeBigShoeImage={() => {}}
-              bigShoeImg=""
+              changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+              bigShoeImg={bigShoeImg}
             />
           </div>
         ))}
