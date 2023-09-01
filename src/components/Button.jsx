8c0591468dfaa6234d62d3c2831cda8d059fd554
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-09-01 18:20:43 +0100

    adding in the button and also the shoe8 from assets/images

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,9 +5,9 @@ const Button = ({ label, iconURL}) => {
     border-coral-red">
         {label}
 
-        <img src={iconURL} alt="arrow right icon"
+       {iconURL && <img src={iconURL} alt="arrow right icon"
             className="ml-2 rounded-full w-5 h-5"
-        />
+        />}
     </button>
   )
 }
