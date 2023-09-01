38db82cf8bb47996e9b1dcb600fdde825ecb4a94
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-09-01 20:52:22 +0100

    finsihing up the review card

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -13,10 +13,16 @@ const CustomerReviews = () => {
         Hear genuine stories from our satisfied customers about their
         exceptional experiences with us.
       </p>
-
+{/* pass in the props into the ReviewCard component */}
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map((review) => (
-          <ReviewCard />
+          <ReviewCard                  
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
         ))}
       </div>
     </section>
