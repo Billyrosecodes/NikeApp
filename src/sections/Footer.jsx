0342e84fbe2a6fc3120c01c175007acabfb00137
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-09-02 00:33:58 +0100

    adding in the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,47 @@
+import { footerLogo } from "../assets/images";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} width={150} height={46} />
+          </a>
+          <p className="mt-16 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike Store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+                <img src={icon.src} 
+                alt={icon.alt} 
+                  width={24}
+                  height={24}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
 
-export default Footer
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+               <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                {section.title}
+               </h4>
+               <ul>
+                {section.links.map((link) => (
+                  <li className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray"></li>
+                ))}
+               </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
