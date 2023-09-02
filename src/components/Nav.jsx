c3c33f6c88370c04993aa081a026022d0b4fdcc8
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-09-02 03:12:02 +0100

    debugging the app

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
             <a href="/">
                 <img src={headerLogo} 
                     alt='Logo'
-                    width={130}
+                    width={129}
                     height={29}
                 />
             </a>
@@ -23,12 +23,14 @@ const Nav = () => {
                 </li>
               ))}
             </ul>
-            <div className='hidden max-lg:block'>
-                <img src={hamburger}
-                 alt="hamburger"
-                 width={25}
-                 height={25} />
-            </div>
+            <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/'>Sign in</a>
+          <span>/</span>
+          <a href='/'>Explore now</a>
+        </div>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        </div>
         </nav>
     </header>
   )
